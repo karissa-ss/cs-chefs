cdf441146d09ece0b36c84b453d161bfb4def625
Ashley Jacob <ashleesjacob@gmail.com>
2022-04-12 11:32:07 -0400

    update

--- script.js
+++ script.js
@@ -7,3 +7,37 @@ function viewRecipe(){
 function hideRecipe() {
     document.getElementById("overlay").style.display = "none";
 }
+
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+ 
+  if (n > slides.length){
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000);
+  // slides[slideIndex-1].style.display = "block";
+  
+}
