4430c4dfbbfa748c13579ee8471f9db769b64499
Ashley Jacob <ashleesjacob@gmail.com>
2022-04-17 15:18:40 -0400

    update

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let url= "https://api.edamam.com/";
 
 async function loadRecipies(){
+  let input=document.getElementById("searchInput").value;
   let app_ID="3ef1ffa1";
   let apiKEY="01ed32d7f78bfefa03baad0adeeda865";
-  let response=await fetch(`${url}search?app_id=${app_ID}&app_key=${apiKEY}&q=pizza`);
+  let response=await fetch(`${url}search?app_id=${app_ID}&app_key=${apiKEY}&q=${input}`);
   let data=await response.json();
 
   console.log(data);
@@ -12,18 +13,24 @@ async function loadRecipies(){
 
 function searchResult(data){
 let html=" ";
+
   for(let item of data.hits){
     html+=`<article class="card" onclick="viewRecipe()">
             <img src=${item.recipe.image}>
             <p class="title">${item.recipe.label}</p>
           </article>`
-    recipeInfo(item);
+
+    
   }
-  document.querySelector("#randomCards").innerHTML=html;
+  // recipeInfo(data.hits[0]);
+  document.querySelector("#displayCards").innerHTML=html; 
+ 
 }
 
 function recipeInfo(data){
-  let details=`<article class="details">
+  let details=" ";
+  console.log(data.recipe.label);
+  details=`<article class="details">
                 <img src=${data.recipe.image}>
                 <h1>${data.recipe.label}</h1>
                 <div class="nutrition">
@@ -48,15 +55,17 @@ function recipeInfo(data){
               </article>`
 
 
-  console.log(data);
+  
 }
 
-function viewRecipe(){
+
+function viewRecipe(){   
     document.getElementById("overlay").style.display = "block";
 }
 function hideRecipe() {
     document.getElementById("overlay").style.display = "none";
 }
+
 
 let slideIndex = 0;
 showSlides(slideIndex);
@@ -85,3 +94,5 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2000);  
 }
+
+
